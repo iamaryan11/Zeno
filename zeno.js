39c341b8543dbcf2b5e5f407bfee4d6b8d8a7eb9
AryanLuv <aryaneos11@gmail.com>
2026-02-05 15:24:01 +0530

    Add a tool to list all the files, however you can also use glob for doing same

--- zeno.js
+++ zeno.js
@@ -8,9 +8,43 @@ const ai=new GoogleGenAI({apiKey:process.env.Zeno_API})
 
 // we need to return file info to LLM like this: App/src/test.js
 async function listFiles({directory}){
+    const files=[];
+    // accept the files only if they have the following extensions
+    const extensions=['.js','.jsx','.ts','.tsx','.html','.css'];
+
+    function scan(dir){
+        const items=fs.readdirSync(dir);
+        for(const item of items){
+            const fullpath=path.join(dir,item);
+
+            // we dont want to give nodemodules, dist, build and files like these:
+            if(fullpath.includes('node_modules')||fullpath.includes('dist')||fullpath.includes('build')){
+                continue;
+            }
+
+            // the following helps us identify whether the the fullpath includes a directory or not:
+            const stat=fs.statSync(fullpath);
+
+            if(stat.isDirectory()){
+                // if it the directory scan it onces again until the level contains files
+                scan(fullpath);
+            }
+            else if(stat.isFile()){
+                const ext=path.extreme(item);
+                if(extensions.includes(ext)){
+                    // we are filling the files empty array only when the fullpath includes the files and not the directory
+                    files.push(fullpath);
+                }
+            }
+        }
+    }
+    scan(directory);
+    console.log(`Found ${files.length} files`);
+    return {files};
 
 }
 
+ 
 
 async function readFile({file_path}){
     const content=fs.readFileSync(file_path,'utf-8');
@@ -24,4 +58,6 @@ async function writeFile({file_path,content}){
     console.log(`Fixed: ${file_path}`);
     return {success:true}
 }
+
+const tools={};
 
